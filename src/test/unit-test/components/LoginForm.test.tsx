8c487333b29914ec94c05b7d8eb9bc59c1db41c4
lonaveer <lonaveer@gmail.com>
2024-03-10 10:06:37 +0700

    lab01-answer-03

--- src/test/unit-test/components/LoginForm.test.tsx
+++ src/test/unit-test/components/LoginForm.test.tsx
@@ -3,28 +3,28 @@ import { LoginForm } from '../../../app/components/LoginForm';
 
 describe('LoginForm', () => {
   test('allows the user to login', async () => {
-    // // เรียกใช้ render LoginForm
-    // xxxxx(xxxxx);
+    // เรียกใช้ render LoginForm
+    render(<LoginForm />);
 
-    // // จำลองการป้อนข้อมูล username และ password
-    // xxxxx.xxxxx(xxxxx);
-    // xxxxx.xxxxx(xxxxx);
+    // จำลองการป้อนข้อมูล username และ password
+    fireEvent.change(screen.getByLabelText(/username/i), { target: { value: 'user' } });
+    fireEvent.change(screen.getByLabelText(/password/i), { target: { value: 'pass' } });
 
-    // // คลิกปุ่ม login
-    // xxxxx.xxxxx(xxxxx);
+    // คลิกปุ่ม login
+    fireEvent.click(screen.getByRole('button', { name: /login/i }));
 
-    // // ตรวจสอบข้อความแสดงผลว่า 'Login Successful'
-    // xxxxx(xxxxx.xxxxx('xxxxx')).xxxxxx(xxxxx');
+    // ตรวจสอบข้อความแสดงผลว่า 'Login Successful'
+    expect(screen.getByTestId('success-message')).toHaveTextContent('Login Successful');
   });
 
   test('shows an error message if the form is submitted with empty fields', () => {
     // // เรียกใช้ render LoginForm
-    // xxxxx(xxxxx);
+    render(<LoginForm />);
 
     // // คลิกปุ่ม login
-    // xxxxx.xxxxx(xxxxx);
+    fireEvent.click(screen.getByRole('button', { name: /login/i }));
 
     // // ตรวจสอบข้อความ error 'Please enter both username and password' เมื่อไม่ได้ป้อนข้อมูล
-    // xxxxx(xxxxx.xxxxx('xxxxx')).xxxxx('xxxxx');
+    expect(screen.getByTestId('error-message')).toHaveTextContent('Please enter both username and password');
   });
 });
